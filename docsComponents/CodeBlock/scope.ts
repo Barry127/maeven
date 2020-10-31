ca4f4885aa7b939604794e3af8b0587f93484f06
Barry de Kleijn <kleijn.barry@gmail.com>
2020-10-31 22:45:18 +0100

    Icon docs

--- docsComponents/CodeBlock/scope.ts
+++ docsComponents/CodeBlock/scope.ts
@@ -1,7 +1,20 @@
+import { circleSolid } from 'icon-packs/fa';
+import {
+  arrowLeftCircle,
+  arrowRightCircle,
+  checkCircle
+} from 'icon-packs/feather';
+import { markGithub, logoGithub } from 'icon-packs/octicons';
 import * as maeven from 'maeven';
 import theme from 'src/themes/maeven';
 
 export const scope = {
   ...maeven,
-  maeven: theme
+  maeven: theme,
+  arrowLeftCircle,
+  arrowRightCircle,
+  checkCircle,
+  circleSolid,
+  logoGithub,
+  markGithub
 };
