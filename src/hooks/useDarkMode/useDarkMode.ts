eac3714964f8fecc8aadc58da3335f27b8e27603
Barry de Kleijn <kleijn.barry@gmail.com>
2020-11-05 20:38:01 +0100

    Update hook docs

--- src/hooks/useDarkMode/useDarkMode.ts
+++ src/hooks/useDarkMode/useDarkMode.ts
@@ -3,6 +3,9 @@ import { createGlobalState } from 'react-hooks-global-state';
 
 const { useGlobalState } = createGlobalState({ isDark: false });
 
+/**
+ * useDarkMode returns wether current theme is dark mode and a function to toggle dark mode.
+ */
 export function useDarkMode(): [boolean, () => void] {
   const [isDark, setDark] = useGlobalState('isDark');
   const toggleDark = () => setDark(!isDark);
